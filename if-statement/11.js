1811dcd2649ebd449e4295f63037bdb7a94cf967
pizzaYami <cyj9407@naver.com>
2022-12-23 20:54:17 +0900

    fst

--- if-statement/11.js
+++ if-statement/11.js
@@ -3,21 +3,21 @@
 //     return false
 //   }
 
-//   let answer
-//   for(let i = 0; i <= str.length-1; i++){
-//     for(let j = 0; j <= str.length-1; j++){
-//       if(i !== j){
-//         if(str[i] === str[j]){
-//           return true
-//         }
-//         // else{ 
-//         //   answer = false
-//         // }
-//       }
-//     }
-//   }
-//   return false
-// }
+  let answer
+  for(let i = 0; i <= str.length-1; i++){
+    for(let j = 0; j <= str.length-1; j++){
+      if(i !== j){
+        if(str[i] === str[j]){
+          return true
+        }
+        // else{ 
+        //   answer = false
+        // }
+      }
+    }
+  }
+  return false
+
 
 // i=0, j=1
 
