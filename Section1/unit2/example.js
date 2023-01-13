93823abad046cdc79a725e6b081106cf2b367d87
pizzaYami <cyj9407@naver.com>
2023-01-13 16:42:40 +0900

    unit2 example

---
+++ Section1/unit2/example.js
@@ -0,0 +1,14 @@
+// ES5
+function Car(brand, name, color){
+  this.brand = brand;
+  this.name = name;
+  this.color = color;
+}
+// ES6
+class Car{
+  constructor(brand, name, color){
+  this.brand = brand;
+  this.name = name;
+  this.color = color;
+  }
+}
