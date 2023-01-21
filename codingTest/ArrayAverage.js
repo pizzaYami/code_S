b8ba6652278c01a21ebeb044a9bcd89a103fd53c
pizzaYami <cyj9407@naver.com>
2023-01-22 00:00:10 +0900

    fdfdfdfd

--- codingTest/ArrayAverage.js
+++ codingTest/ArrayAverage.js
@@ -2,6 +2,7 @@ function solution(numbers) {
   for(i=0; i<numbers.length; i++){
       let result = 0
       console.log(i)
+      console.log(k)
       result += numbers[i]
   }
   return result / numbers.length
